2e9533444addfa511268116f2681b2a7f0c28db9
Rohith <cs21btech11062@iith.ac.in>
2022-06-19 22:22:54 +0530

    THIS IS AM MESS
    DESCRIPTION

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav className="navbar" style={{backgroundColor: 'rgb(125, 13, 211)'}}>
+     <div className='title'>Tic Tac Toe</div>
+</nav>
+</>
   );
 }
 
