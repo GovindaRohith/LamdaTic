e774e950d872f9c40287079cddf8e424d8981291
Rohith <cs21btech11062@iith.ac.in>
2022-06-19 22:58:02 +0530

    THIS IS AM MESS
    DESCRIPTION

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,8 +4,10 @@ function App() {
   return (
     <>
     <nav className="navbar" style={{backgroundColor: 'rgb(125, 13, 211)'}}>
-     <div className='title'>Tic Tac Toe</div>
+<div className='title'>Tic Tac Toe</div>
 </nav>
+<button type="button" class="btn btn-outline-dark">Start 1P vs AI</button>
+<button type="button" class="btn btn-outline-dark">Start 2 PvP</button>
 </>
   );
 }
